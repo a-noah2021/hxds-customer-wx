10d7df9b0f499c5f4737dc814a37d6687163f781
noah2021 <noah2021@foxmail.com>
2023-02-13 00:02:45 +0800

    :sparkles: 实现取消下单

--- pages/create_order/create_order.vue
+++ pages/create_order/create_order.vue
@@ -41,6 +41,7 @@
 			</view>
 			<button class="btn" @tap="createOrderHandle">确定下单</button>
 		</view>
+		<u-popup v-model="infoStatus" mode="center" width="600" height="350"><view>出淤泥而不染，濯清涟而不妖</view></u-popup>
 		<u-popup v-model="showPopup" mode="center" width="600" height="350" :mask-close-able="false">
 			<view class="popup-title">您的订单正在等待司机接单</view>
 			<view class="count-down">
@@ -89,11 +90,11 @@ export default {
 			markers: [],
 			infoStatus: true,
 			carId: null,
-			carPlate: null,	// 车牌
+			carPlate: null, // 车牌
 			carType: null,
 			showCar: false,
 			showPopup: false,
-			timestamp: 15 * 60,
+			timestamp: 60,
 			orderId: null
 		};
 	},
@@ -116,135 +117,194 @@ export default {
 			return pl;
 		},
 		calculateLine: function(ref) {
-		    qqmapsdk.direction({
-		        mode: 'driving',
-		        from: {
-		            latitude: ref.from.latitude,
-		            longitude: ref.from.longitude
-		        },
-		        to: {
-		            latitude: ref.to.latitude,
-		            longitude: ref.to.longitude
-		        },
-		        success: function(resp) {
-		            if (resp.status != 0) {
-		                uni.showToast({
-		                    icon: 'error',
-		                    title: resp.message
-		                });
-		                return;
-		            }
-		            let route = resp.result.routes[0];
-		            let distance = route.distance;
-		            let duration = route.duration;
-		            let polyline = route.polyline;
-		            ref.distance = Math.ceil((distance / 1000) * 10) / 10;
-		            ref.duration = duration;
-		            let points = ref.formatPolyline(polyline);
-		
-		            ref.polyline = [
-		                {
-		                    points: points,
-		                    width: 6,
-		                    color: '#05B473',
-		                    arrowLine: true
-		                }
-		            ];
-		            ref.markers = [
-		                {
-		                    id: 1,
-		                    latitude: ref.from.latitude,
-		                    longitude: ref.from.longitude,
-		                    width: 25,
-		                    height: 35,
-		                    anchor: {
-		                        x: 0.5,
-		                        y: 0.5
-		                    },
-		                    iconPath: 'https://mapapi.qq.com/web/lbs/javascriptGL/demo/img/start.png'
-		                },
-		                {
-		                    id: 2,
-		                    latitude: ref.to.latitude,
-		                    longitude: ref.to.longitude,
-		                    width: 25,
-		                    height: 35,
-		                    anchor: {
-		                        x: 0.5,
-		                        y: 0.5
-		                    },
-		                    iconPath: 'https://mapapi.qq.com/web/lbs/javascriptGL/demo/img/end.png'
-		                }
-		            ];
-		        }
-		    });
+			qqmapsdk.direction({
+				mode: 'driving',
+				from: {
+					latitude: ref.from.latitude,
+					longitude: ref.from.longitude
+				},
+				to: {
+					latitude: ref.to.latitude,
+					longitude: ref.to.longitude
+				},
+				success: function(resp) {
+					if (resp.status != 0) {
+						uni.showToast({
+							icon: 'error',
+							title: resp.message
+						});
+						return;
+					}
+					let route = resp.result.routes[0];
+					let distance = route.distance;
+					let duration = route.duration;
+					let polyline = route.polyline;
+					ref.distance = Math.ceil((distance / 1000) * 10) / 10;
+					ref.duration = duration;
+					let points = ref.formatPolyline(polyline);
+
+					ref.polyline = [
+						{
+							points: points,
+							width: 6,
+							color: '#05B473',
+							arrowLine: true
+						}
+					];
+					ref.markers = [
+						{
+							id: 1,
+							latitude: ref.from.latitude,
+							longitude: ref.from.longitude,
+							width: 25,
+							height: 35,
+							anchor: {
+								x: 0.5,
+								y: 0.5
+							},
+							iconPath: 'https://mapapi.qq.com/web/lbs/javascriptGL/demo/img/start.png'
+						},
+						{
+							id: 2,
+							latitude: ref.to.latitude,
+							longitude: ref.to.longitude,
+							width: 25,
+							height: 35,
+							anchor: {
+								x: 0.5,
+								y: 0.5
+							},
+							iconPath: 'https://mapapi.qq.com/web/lbs/javascriptGL/demo/img/end.png'
+						}
+					];
+				}
+			});
 		},
-		chooseCarHandle: function(){
+		chooseCarHandle: function() {
 			uni.navigateTo({
 				url: '../car_list/car_list'
-			})
+			});
 		},
-    createOrderHandle: function() {
-        let that = this;
-        if (that.carType == null || that.carPlate == null) {
-            uni.showToast({
-                icon: 'error',
-                title: '没有设置代驾车辆'
-            });
-            return;
-        }
-        uni.showLoading({
-            title: '下单中请稍后'
-        });
-        setTimeout(function() {
-            uni.hideLoading();
-        }, 60000);
-        let data = {
-            startPlace: that.from.address,
-            startPlaceLatitude: that.from.latitude,
-            startPlaceLongitude: that.from.longitude,
-            endPlace: that.to.address,
-            endPlaceLatitude: that.to.latitude,
-            endPlaceLongitude: that.to.longitude,
-            favourFee: '0.0',
-            carPlate: that.carPlate,
-            carType: that.carType
-        };
-        that.ajax(that.url.createNewOrder, 'POST', data, function(resp) {
-            uni.hideLoading();
-            if (resp.data.result.count > 0) {
-                uni.showToast({
-                    icon: 'success',
-                    title: '订单创建成功'
-                });
-                setTimeout(function() {
-                    that.orderId = resp.data.result.orderId;
-                    that.showPopup = true;
-                    //此处应该是15*60，但是测试中我们等不了15分钟,每隔5分钟发送一次查询接单情况的请求
-                    /*that.timestamp = 60;
-                    that.$refs.uCountDown.start();*/
-                }, 2000);
-            } else {
-                uni.showToast({
-                    icon: 'none',
-                    title: '没有适合接单的司机'
-                });
-            }
-        });
-    },
+		createOrderHandle: function() {
+			let that = this;
+			if (that.carType == null || that.carPlate == null) {
+				uni.showToast({
+					icon: 'error',
+					title: '没有设置代驾车辆'
+				});
+				return;
+			}
+			uni.showLoading({
+				title: '下单中请稍后'
+			});
+			setTimeout(function() {
+				uni.hideLoading();
+			}, 60000);
+			let data = {
+				startPlace: that.from.address,
+				startPlaceLatitude: that.from.latitude,
+				startPlaceLongitude: that.from.longitude,
+				endPlace: that.to.address,
+				endPlaceLatitude: that.to.latitude,
+				endPlaceLongitude: that.to.longitude,
+				favourFee: '0.0',
+				carPlate: that.carPlate,
+				carType: that.carType
+			};
+			that.ajax(that.url.createNewOrder, 'POST', data, function(resp) {
+				uni.hideLoading();
+				if (resp.data.result.count > 0) {
+					uni.showToast({
+						icon: 'success',
+						title: '订单创建成功'
+					});
+					setTimeout(function() {
+						that.orderId = resp.data.result.orderId;
+						that.showPopup = true;
+						//此处应该是15*60，但是测试中我们等不了15分钟,每隔5分钟发送一次查询接单情况的请求
+						that.timestamp = 60;
+						that.$refs.uCountDown.start();
+					}, 2000);
+				} else {
+					uni.showToast({
+						icon: 'none',
+						title: '没有适合接单的司机'
+					});
+				}
+			});
+		},
+		searchOrderStatus: function(ref) {
+		   let data = {
+		      orderId: ref.orderId
+		   };
+		   ref.ajax(
+		           ref.url.searchOrderStatus,
+		           'POST',
+		           data,
+		           function(resp) {
+		              if (resp.data.result == 2) {
+		                 ref.showPopup = false;
+		                 ref.timestamp = null;
+		                 uni.showToast({
+		                    icon: 'success',
+		                    title: '司机已接单'
+		                 });
+		                 uni.vibrateShort({});
+		                 setTimeout(function() {
+		                    uni.redirectTo({
+		                       url: '../move/move?orderId=' + ref.orderId
+		                    });
+		                 }, 3000);
+		              }
+		              else if (resp.data.result == 0) {
+		                 ref.showPopup = false;
+		                 ref.timestamp = null;
+		                 uni.showToast({
+		                    icon: 'success',
+		                    title: '订单已经关闭'
+		                 });
+		              }
+		           },
+		           false
+		   );
+		},
+		deleteUnAcceptOrder: function(ref) {
+		   ref.showPopup = false;
+		   ref.timestamp = null;
+		   let data = {
+		      orderId: ref.orderId
+		   };
+		   ref.ajax(ref.url.deleteUnAcceptOrder, 'POST', data, function(resp) {
+		      let result = resp.data.result;
+		      console.log(result);
+		      if (result == '订单取消成功') {
+		         uni.showToast({
+		            icon: 'success',
+		            title: '订单取消成功'
+		         });
+		         setTimeout(function() {
+		            uni.redirectTo({
+		               url: '../workbench/workbench'
+		            });
+		         }, 3000);
+		      } else {
+		         ref.searchOrderStatus(ref);
+		      }
+		   });
+		},
 		countChangeHandle: function(s) {
 			let that = this;
 			if (s != 0 && s % 5 == 0) {
-				// that.searchOrderStatus(that);
+				that.searchOrderStatus(that);
 			}
 		},
 		countEndHandle: function() {
 			let that = this;
-			// that.deleteUnAcceptOrder(that);
+			that.deleteUnAcceptOrder(that);
 		},
 		cancelHandle: function() {
 			let that = this;
-			// that.deleteUnAcceptOrder(that);
+			that.deleteUnAcceptOrder(that);
 		}
 	},
 	onLoad: function(options) {
@@ -256,17 +316,24 @@ export default {
 
 		that.from = uni.getStorageSync('from');
 		that.to = uni.getStorageSync('to');
-		
+
 		qqmapsdk = new QQMapWX({
 			key: that.tencent.map.key
 		});
 		that.map = uni.createMapContext('map');
-		
-		if(options.hasOwnProperty('showCar')){
+
+		if (options.hasOwnProperty('showCar')) {
 			that.showCar = options.showCar;
 			that.carId = options.carId;
 			that.carPlate = options.carPlate;
-			this.carType = options.carType;
+			that.carType = options.carType;
+		}
+		
+		if(options.hasOwnProperty("showPopup")){
+			that.timestamp=60
+			that.showPopup=true
+			that.orderId=options.orderId
+			that.$refs.uCountDown.start();
 		}
 	},
 	onShow: function() {
